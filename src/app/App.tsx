618f81d4948b34f6e844319d11920f0c710826df
Johan Rytterfelt <johanrytterfelt@gmail.com>
2021-03-12 23:27:05 +0100

    fix(App): Basic structural changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,8 +7,21 @@
 /**
  * Imports.
  */
+import BemHelper     from 'react-bem-helper';
 import React, { FC } from 'react';
 import './App.pcss';
+
+
+
+/**
+ * Constants.
+ */
+const bemHelper: BemHelper<string> = new BemHelper(
+	{
+		name          : 'app',
+		outputIsString: true,
+	}
+);
 
 
 
@@ -18,7 +31,7 @@ import './App.pcss';
 const App: FC = () =>
 {
 	return (
-		<div>
+		<div className={ bemHelper() }>
 			{ 'My name is App, nice to meet you!' }
 		</div>
 	);
