98066fa91b99d6b7ab4b7946e3dc62de3ae2c5b4
Johan Rytterfelt <johanrytterfelt@gmail.com>
2021-03-13 23:34:50 +0100

    fix(Build): Added a generic file to hold all our variables and implemented it into PostCSS variables (we do it like this to enable variable sharing regardless of language as this allows us to read the file from the backend even if we decide not to go for JS which we ccould handle via Webpack, but instead go for PHP or some C-based variant)

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,71 @@
+/**
+ * Config for PostCSS.
+ */
+
+
+
+/**
+ * Imports.
+ */
+const variables = require( './variables.json' );
+
+
+
+/**
+ * Function for processing variables to allow recursion.
+ *
+ * @param object input Input to process.
+ */
+function processVariables( input, collectedVariables = {}, nestedKeyName = '' )
+{
+	for ( const key in input )
+	{
+		let newNestedKeyName;
+
+		if ( nestedKeyName )
+		{
+			newNestedKeyName = nestedKeyName + key.charAt( 0 ).toUpperCase() + key.slice( 1 );
+		}
+		else
+		{
+			newNestedKeyName = key;
+		}
+
+		if ( input[ key ] && 'object' === typeof input[ key ] && ! ( input[ key ] instanceof Array ) )
+		{
+
+			processVariables(
+				input[ key ],
+				collectedVariables,
+				newNestedKeyName,
+			);
+		}
+		else
+		{
+			collectedVariables[ newNestedKeyName ] = input[ key ];
+		}
+	}
+
+	return collectedVariables;
+}
+
+
+
+/**
+ * Main config object.
+ */
 module.exports =
 {
 	plugins:
-	[
-		'postcss-css-variables',
-		'postcss-nested',
-		'postcss-nested-props',
-		'postcss-nested-ancestors',
-		'postcss-preset-env',
-		'postcss-property-lookup',
-	],
+	{
+		'postcss-css-variables':
+		{
+			variables: processVariables( variables ),
+		},
+		'postcss-nested'          : {},
+		'postcss-nested-props'    : {},
+		'postcss-nested-ancestors': {},
+		'postcss-preset-env'      : {},
+		'postcss-property-lookup' : {},
+	},
 };
