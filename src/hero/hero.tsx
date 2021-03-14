e058be6ed1f3a047411c5a7aefc3f25282a3eb1c
Johan Rytterfelt <johanrytterfelt@gmail.com>
2021-03-14 02:59:15 +0100

    feat(Translation): Added basic language picker

--- src/hero/hero.tsx
+++ src/hero/hero.tsx
@@ -7,9 +7,11 @@
 /**
  * Imports.
  */
-import { FC }      from 'react';
-import { Section } from 'section/section';
-import BemHelper   from 'react-bem-helper';
+import { FC }             from 'react';
+import { LanguagePicker } from 'languagePicker/languagePicker';
+import { Section }        from 'section/section';
+import { Text }           from 'text/text';
+import BemHelper          from 'react-bem-helper';
 import './hero.pcss';
 
 
@@ -32,9 +34,10 @@ const bemHelper: BemHelper<string> = new BemHelper(
 export const Hero: FC = () =>
 (
 	<Section className={ bemHelper() } position='first'>
-		<h1 className={ bemHelper( 'heading' ) }>
+		<Text className={ bemHelper( 'heading' ) } color='light' size='h2' tag='h1'>
 			{ 'TODO: Add heading here' }
-		</h1>
+		</Text>
+		<LanguagePicker className={ bemHelper( 'languagePicker' ) } />
 		<div className={ bemHelper( 'portrait' ) } />
 		<div className={ bemHelper( 'portraitBackground' ) } />
 	</Section>
---
+++ src/languagePicker/languagePicker.tsx
@@ -0,0 +1,137 @@
+/**
+ * Module for the skills section.
+ */
+
+
+
+/**
+ * Imports.
+ */
+import { AppContext }         from 'app/app';
+import {
+	FC,
+	MouseEvent,
+	useCallback,
+	useContext,
+}                             from 'react';
+// @ts-ignore
+import { GB as Gb, SE as Se } from 'country-flag-icons/react/3x2';
+import { LanguagesType }      from 'translations/translations';
+import { Text }               from 'text/text';
+import BemHelper              from 'react-bem-helper';
+import './languagePicker.pcss';
+
+
+
+/**
+ * Types.
+ */
+type LanguagePickerPropsType =
+{
+	className?: string;
+};
+
+
+
+/**
+ * Constants.
+ */
+const bemHelper: BemHelper<string> = new BemHelper(
+	{
+		name          : 'languagePicker',
+		outputIsString: true,
+	}
+);
+
+
+
+/**
+ * React component.
+ */
+export const LanguagePicker: FC<LanguagePickerPropsType> = ( { className }: LanguagePickerPropsType ) =>
+{
+	const { language, setLanguage } = useContext( AppContext );
+
+	const handleLanguageChange = useCallback(
+		( evt: MouseEvent<HTMLButtonElement> ): void =>
+		{
+			setLanguage(
+				evt.currentTarget.getAttribute( 'data-lang' ) as LanguagesType
+			);
+		},
+		[
+			setLanguage,
+		]
+	);
+
+	return (
+		<div
+			className={
+				bemHelper(
+					{
+						extra: className,
+					}
+				)
+			}
+		>
+			<button
+				aria-label='Change language to english'
+				className={
+					bemHelper(
+						{
+							element  : 'button',
+							modifiers:
+							{
+								active: 'en' === language,
+							},
+						}
+					)
+				}
+				data-lang='en'
+				onClick={ handleLanguageChange }
+				type='button'
+			>
+				<Gb className={ bemHelper( 'flag' ) } />
+				<Text
+					align='center'
+					className={ bemHelper( 'text' ) }
+					color='light'
+					noMargins
+					size='caption'
+					tag='span'
+				>
+					{ 'En' }
+				</Text>
+			</button>
+			<button
+				aria-label='Byt språk till svenska'
+				className={
+					bemHelper(
+						{
+							element  : 'button',
+							modifiers:
+							{
+								active: 'sv' === language,
+							},
+						}
+					)
+				}
+				data-lang='sv'
+				onClick={ handleLanguageChange }
+				type='button'
+			>
+				<Se className={ bemHelper( 'flag' ) } />
+				<Text
+					align='center'
+					className={ bemHelper( 'text' ) }
+					color='light'
+					noMargins
+					size='caption'
+					tag='span'
+				>
+					{ 'Sv' }
+				</Text>
+			</button>
+		</div>
+	);
+};
